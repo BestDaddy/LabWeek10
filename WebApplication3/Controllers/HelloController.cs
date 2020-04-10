00e5de19db5c7724ad0d781a06acff3a0bc6a4f0
User <User@USER->
2020-04-10 23:09:12 +0600

    Добавьте файлы проекта.

---
+++ WebApplication3/Controllers/HelloController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HelloController : Controller
+    {
+
+        public IActionResult Index(string code)
+        {
+            Dictionary<string, string[]> students = new Dictionary<string, string[]>(10);
+            students.Add("1704BB99", new string[2] { "Beka Bershimbekov",  "dovn"});
+            students.Add("1704AT99", new string[2] { "Azamat Tleubekov", "dovn" });
+            students.Add("1704AK99", new string[2] { "Azamat Kenzhebayev", "dovn" });
+
+            if (!string.IsNullOrEmpty(code))
+            {
+                string result = "";
+                foreach (KeyValuePair<string, string[]> keyValue in students)
+                {
+                    if (keyValue.Key.Equals(code))
+                    {
+                        result = result + keyValue.Value[0];
+                    }
+                    
+                }
+                ViewData["Message"] = result;
+            }
+            //else if (code.Equals("1704BB99"))
+            //{
+            //    string result = "";
+            //    foreach (KeyValuePair<string, string[]> keyValue in students)
+            //    {
+            //        result = result +  keyValue.Value[0];
+            //    }
+            //    ViewData["Message"] = result;
+            //}
+            else
+            {
+                    
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Controllers/TestController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class TestController : Controller
+    {
+
+        Dictionary<string, string[]> students = new Dictionary<string, string[]>(10);
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+           
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string name, string group, string date, string search)
+        {
+            if (string.IsNullOrEmpty(search))
+            {
+                string authData = $"Name: {name}   Group: {group}   Date: {date}";
+                string code = "";
+
+
+                code = code + date[2] + date[3];
+
+
+                string[] name1 = name.Split(new char[] { ' ' });
+
+                foreach (string s in name1)
+                {
+                    code = code + s[0];
+                }
+                Console.WriteLine(code);
+
+                code = code + group;
+
+                //code = code + 
+                //students.Add("1704BB99", new string[2] { "Beka Bershimbekov", "dovn" });
+
+                students.Add(code, new string[3] { name, group, date });
+                ViewData["Message"] = authData + " Code: " + code;
+
+                return View();
+            }
+            else
+            {
+                string result = "";
+                foreach (KeyValuePair<string, string[]> keyValue in students)
+                {
+                    if (keyValue.Key.Equals(search))
+                    {
+                        result = result + keyValue.Value[0];
+                    }
+
+                }
+                ViewData["Message"] = result+ "hz";
+
+                return View();
+
+            }
+            
+            // Content(authData);
+        }
+    }
+}
